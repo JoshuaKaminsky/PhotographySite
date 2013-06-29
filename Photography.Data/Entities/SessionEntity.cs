ecd3b41ad6261b694f3ed17b326ebf107b632f19
unknown <rms@HMBLATX0114.hmbnet.hmb>
2013-06-29 18:50:29 -0400

    add user session role classes

---
+++ Photography.Data/Entities/SessionEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Photography.Data.Entities
+{
+    internal class SessionEntity
+    {
+        public int Id { get; set; }
+
+        public Guid SessionKey { get; set; }
+
+        public int UserId { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+    }
+}
---
+++ Photography.Data/Entities/UserEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Photography.Data.Entities
+{
+    internal class UserEntity
+    {
+        public int Id { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public string Name { get; set; }
+
+        public string Password { get; set; }
+
+        public string Salt { get; set; }
+    }
+}
