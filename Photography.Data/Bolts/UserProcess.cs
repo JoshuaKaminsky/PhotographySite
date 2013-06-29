8ebe17129cb2dab3d03402c616de4bcf0b4a1390
unknown <rms@HMBLATX0114.hmbnet.hmb>
2013-06-29 19:24:23 -0400

    UserProcess class

---
+++ Photography.Data/Bolts/UserProcess.cs
@@ -0,0 +1,14 @@
+﻿using Photography.Core.Contracts.Process;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Photography.Data.Bolts
+{
+    public class UserProcess : IUserProcess
+    {
+
+    }
+}
