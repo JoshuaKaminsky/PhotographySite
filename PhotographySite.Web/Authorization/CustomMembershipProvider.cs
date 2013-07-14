d74ecee917f78728852633d18d9a92e5e1016263
joshuakaminsky <josh.kaminsky@gmail.com>
2013-07-14 00:09:56 -0400

    added session service to membership provider

--- PhotographySite.Web/Authorization/CustomMembershipProvider.cs
+++ PhotographySite.Web/Authorization/CustomMembershipProvider.cs
@@ -1,20 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using Photography.Core.Contracts;
-using Photography.Core.Contracts.Service;
+﻿using Photography.Core.Contracts.Service;
 using WebMatrix.WebData;
 
 namespace PhotographySite.Authorization
 {
     public class CustomMembershipProvider : SimpleMembershipProvider
     {
-        public CustomMembershipProvider(IUserService userService)
+        private readonly IUserService _userService;
+        private readonly ISessionService _sessionService;
+
+        public CustomMembershipProvider(IUserService userService, ISessionService sessionService)
         {
-            
+            _userService = userService;
+            _sessionService = sessionService;
         }
-        
+
         public override System.Web.Security.MembershipUser CreateUser(string username, string password, string email, string passwordQuestion, string passwordAnswer, bool isApproved, object providerUserKey, out System.Web.Security.MembershipCreateStatus status)
         {
             //return base.CreateUser(username, password, email, passwordQuestion, passwordAnswer, isApproved, providerUserKey, out status);
