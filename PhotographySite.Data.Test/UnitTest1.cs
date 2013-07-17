5286dae506f18dde4cfb05ac7a6ad9d123a4878f
joshuakaminsky <josh.kaminsky@gmail.com>
2013-07-17 17:34:05 -0400

    additional unit tests

---
+++ PhotographySite.Data.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Photography.Data;
+using Photography.Data.Bolts;
+
+namespace PhotographySite.Data.Test
+{
+    [TestClass]
+    public class DataTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var userProcess = new UserProcess(new BaseUnitOfWork(new RepositoryProvider(new RepositoryFactories())));
+            var user = userProcess.CreateUser("JoshKaminsky@gmail.com", "p");
+        }
+    }
+}
