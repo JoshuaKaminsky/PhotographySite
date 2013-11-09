ad1e1b691aaefe6570cca548b0fe78d9ab80606c
joshuakaminsky <josh.kaminsky@gmail.com>
2013-11-09 18:26:03 -0500

    added fontawesome and bootstrap

--- PhotographySite.Web/App_Start/BootstrapBundleConfig.cs
+++ PhotographySite.Web/App_Start/BootstrapBundleConfig.cs
@@ -13,7 +13,10 @@ namespace PhotographySite.App_Start
 			// Add @Scripts.Render("~/bundles/bootstrap") after jQuery in your _Layout.cshtml view
 			// When <compilation debug="true" />, MVC4 will render the full readable version. When set to <compilation debug="false" />, the minified version will be rendered automatically
 			BundleTable.Bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));
-			BundleTable.Bundles.Add(new StyleBundle("~/Content/bootstrap/base").Include("~/Content/bootstrap/bootstrap.css"));
+		    BundleTable.Bundles.Add(new StyleBundle("~/Content/bootstrap/base")
+		                                .Include("~/Content/bootstrap/bootstrap.css")
+		                                .Include("~/Content/fontawesome/font-awesome.css"));
+
 			BundleTable.Bundles.Add(new StyleBundle("~/Content/bootstrap/theme").Include("~/Content/bootstrap/bootstrap-theme.css"));
 		}
 	}
